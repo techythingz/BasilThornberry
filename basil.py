82fb9484384e72de08c4041d0bec1e5d0379a2e0
aoa4eva <afua.ankomah@gmail.com>
2017-09-01 04:10:52 +0000

    Modified basil.py

--- basil.py
+++ basil.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 app = Flask(__name__)
 app.debug = True
 port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0') 
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 

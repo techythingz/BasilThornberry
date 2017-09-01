8488d50f042030bee490060b0199319d4a4c3563
aoa4eva <afua.ankomah@gmail.com>
2017-09-01 03:09:36 +0000

    Modified basil.py

--- basil.py
+++ basil.py
@@ -4,12 +4,12 @@ from flask import render_template
 from flask import request 
 from flask import session 
 from flask import url_for, redirect, escape
-if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
+app = Flask(__name__)
+app.debug = True
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route("/")
 def hello():
@@ -69,7 +69,7 @@ def connektBasilR():
 
 	for table in alltables:
 	    stripped = "%s" % table
-   		theList.append(stripped)
+	    theList.append(stripped)
  
 	cursor.close()	    	    
 	session['alltables']=theList
